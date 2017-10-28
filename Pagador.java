d7472ec01bb775c5cb31f9c39b3e5f8020c12c50
Jose A. Alvarez <riboflavina_@hotmail.com>
2017-10-28 13:08:00 +0200

    Agrego Pagador.java

---
+++ Pagador.java
@@ -0,0 +1,13 @@
+package Comunio;
+
+import java.util.Scanner;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class Pagador{
+	
+//Sistema de pago automático a implementar
+	
+}
